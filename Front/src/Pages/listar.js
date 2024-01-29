8cf8e9b2b41a00b918ca5d66462dd8f9f8773d69
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-01-29 13:57:31 -0300

    filtro na tela de listar

--- Front/src/Pages/listar.js
+++ Front/src/Pages/listar.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 function ListarClientes() {
     const [data, setData] = useState(null);
+    const [filtro, setFiltro] = useState('');
+    const [clientesFiltrados, setClientesFiltrados] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -17,10 +19,31 @@ function ListarClientes() {
         fetchData();
     }, []);
 
+    const handleFiltroChange = (e) => {
+        const novoFiltro = e.target.value.toLowerCase();
+        setFiltro(novoFiltro);
+
+        const clientesFiltrados = data.filter((cliente) => {
+            return (
+                cliente.nome.toLowerCase().includes(novoFiltro) ||
+                cliente.email.toLowerCase().includes(novoFiltro) ||
+                cliente.telefone.toLowerCase().includes(novoFiltro)
+            );
+        });
+
+        setClientesFiltrados(clientesFiltrados);
+    };
+
     return (
         <div>
             <h1>Clientes:</h1>
-            {data && (
+            <input
+                type="text"
+                placeholder="Filtrar por nome, email ou telefone"
+                value={filtro}
+                onChange={handleFiltroChange}
+            />
+            {clientesFiltrados && (
                 <table>
                     <thead>
                         <tr>
@@ -31,7 +54,7 @@ function ListarClientes() {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map((item) => (
+                        {clientesFiltrados.map((item) => (
                             <tr key={item.id}>
                                 <td>{item.id}</td>
                                 <td>{item.nome}</td>
