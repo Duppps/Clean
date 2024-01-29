2a46c1199c63e3e4ecb38d635904629347d29395
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-01-29 11:35:07 -0300

    alterada estrutura API

--- API/src/index.js
+++ API/src/index.js
@@ -1,62 +1,33 @@
+const { Pool } = require('pg');
 const express = require('express');
-const { Client } = require('pg');
 const app = express();
 const port = 3000;
 
-app.get('/clientes', (req, res) => {
-    const client = new Client({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'clean',
-        password: 'sht@#$X89',
-        port: 5432,
-    });
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'clean',
+    password: 'sht@#$X89',
+    port: 5432,
+});    
 
-    client.connect();
+app.get('/clientes', async (req, res) => {
+    const client = await pool.connect();
 
-    const query = 'SELECT * FROM clientes';
-
-    client.query(query, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Erro ao executar a consulta.');
-            return;
-        }
-
-        console.log(result.rows);
-
+    try {
+        const query = 'SELECT * FROM clientes';
+        const result = await client.query(query);
         res.json(result.rows);
-
-        client.end();
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao executar a consulta.');
+        return;
+    } finally {
+        client.release();
+    }
 });
 
 app.post('/clientes', (req, res) => {
-    const client = new Client({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'clean',
-        password: 'sht@#$X89',
-        port: 5432,
-    });
-
-    client.connect();
-
-    const query = 'INSERT INTO CLIENTES VALUES ';
-
-    client.query(query, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Erro ao executar a consulta.');
-            return;
-        }
-
-        console.log(result.rows);
-
-        res.json(result.rows);
-
-        client.end();
-    });
 });
 
 app.listen(port, () => {
